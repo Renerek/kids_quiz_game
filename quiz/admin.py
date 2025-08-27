7d1b912bc8749633bdda410e9a44e159d51b4845
Rene Rek <ntumngiarene@gmail.com>
2025-08-27 00:20:30 -0400

    fixing

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,5 @@
-
 from django.contrib import admin
+
 from .models import MathQuestion, QuizSession, UserProgress
 
 admin.site.register(MathQuestion)
--- quiz/urls.py
+++ quiz/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'quiz'
+app_name = "quiz"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('start/', views.start_quiz, name='start'),
-    path('question/', views.question, name='question'),
-    path('submit/', views.submit_answer, name='submit_answer'),
-    path('spelling/', views.spelling_game, name='spelling'),
-    path('what-time/', views.what_time_is_it, name='what_time'),
-    path('basic-questions/', views.basic_questions, name='basic_questions'),
+    path("", views.home, name="home"),
+    path("start/", views.start_quiz, name="start"),
+    path("question/", views.question, name="question"),
+    path("submit/", views.submit_answer, name="submit_answer"),
+    path("spelling/", views.spelling_game, name="spelling"),
+    path("what-time/", views.what_time_is_it, name="what_time"),
+    path("basic-questions/", views.basic_questions, name="basic_questions"),
 ]
